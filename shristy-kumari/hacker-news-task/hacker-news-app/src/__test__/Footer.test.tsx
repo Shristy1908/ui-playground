566680fa818c3d610f57b67d0d4fbaaf28f970dd
Shristy Kumari <shristykumari@Shristys-MacBook-Air.local>
2025-04-03 12:44:14 +0530

    Removed hacker-news-task from the pull request

--- shristy-kumari/hacker-news-task/hacker-news-app/src/__test__/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render,screen } from "@testing-library/react";
-import Footer from "../Components/Footer"
-
-describe("Footer Component", () => {
-  test("renders the footer with correct text",()=>{
-     render(<Footer/>)
-     expect(screen.getByText("HACKERNEWS.")).toBeInTheDocument()
-  })
-})
